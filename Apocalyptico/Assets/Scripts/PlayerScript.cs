26c9f9f9a5d0d97a67fd62b5861e0496995628f4
Skywonder <masterkuanthun@hotmail.com>
2016-10-17 19:33:44 -0700

    Test commit

---
+++ Apocalyptico/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //working
+        Debug.Log("This is testing for player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
