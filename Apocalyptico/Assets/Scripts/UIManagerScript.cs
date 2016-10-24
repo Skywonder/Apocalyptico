aed4dd873127542474e8512590d7c339f979efa2
John J <jejuarez88@gmail.com>
2016-10-24 13:08:24 -0700

    Added Audio File To Menu & LoadLevel/Exit Script

---
+++ Apocalyptico/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGame () {
+
+        Application.LoadLevel("Kuan (working scene)");
+	}
+	
+	// Update is called once per frame
+	public void ExitGame () {
+        Application.Quit();
+	}
+}
