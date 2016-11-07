95eb3b45f026dd9065e2464a74412567a5bc4be0
Skywonder <masterkuanthun@hotmail.com>
2016-11-07 01:16:30 -0800

    Some Test Stage changes
    Feel free to try it out
    1. Can jump on wall thanks to the side effect of collider position
    checker (simply remove ground tag from the wall then cant jump no more)
    2. slope modification to less than 20degree makes walking down hill more
    natural
    3. an enemy object with 20hp for dmg testing from player
    4. Fix animation flow
    5. bullet scripts and adjustments
    6. adds my version of test prefabs

---
+++ Apocalyptico/Assets/Scripts/Kuan(New Script)/PlayerGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerGUI : MonoBehaviour {
+
+    public GameObject player;
+    private int playerhp;
+    private int maxhp;
+    Text text;
+
+	// Use this for initialization
+	void Awake () {
+        maxhp = (int)player.GetComponent<PlayerScript>().getMaxHealth();
+        playerhp = (int)player.GetComponent<PlayerScript>().getCurHealth();
+        playerhp = maxhp;
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+  
+        text.text = "Player Health: " + playerhp;
+	}
+}
