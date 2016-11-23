a6e3017f842b5d0033948f89bfbc220dece8970b
Rikerd <cursedmarkedsasuke@gmail.com>
2016-11-23 01:08:39 -0800

    New Movement
    Using a copied movement code that detects all collision through
    raycasting...
    RigidBody no longer needed with this code

---
+++ Apocalyptico/Assets/Scripts/Modified Demo Scripts/RichardPlayer.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RichardPlayer : MonoBehaviour
+{
+    public int hp = 5;
+
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 10;
+
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    RichardController controller;
+
+    void Start()
+    {
+        controller = GetComponent<RichardController>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+    }
+
+    void Update()
+    {
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+
+        if (velocity.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
---
+++ Apocalyptico/Assets/Scripts/Modified Demo Scripts/Walker2D.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walker2D : MonoBehaviour {
+    public int hp = 1;
+    
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 3;
+
+    float gravity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    RichardController controller;
+
+    private Transform player;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<RichardController>();
+
+        gravity = -10;
+
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move;
+        
+        if (player.position.x > transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            move = new Vector2(moveSpeed, gravity);
+        } else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            move = new Vector2(-moveSpeed, gravity);
+        }
+
+        controller.Move(move * Time.deltaTime);
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
