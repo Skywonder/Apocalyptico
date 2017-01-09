456444dd440766a647ef8620bc4e7576cf948014
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-09 00:29:05 -0800

    Shooter + Shooter Bullet
    Shooter and shooter bullet have been updated to work with current demo.
    (currently shooter should only be used on flat surfaces... cannot
    properly function on slopes)

---
+++ Apocalyptico/Assets/Scripts/Demo Scripts/Enemy Scripts/EnemyBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+    public bool left;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector2 direction;
+
+        if (left)
+        {
+            direction = new Vector2(-1.0f, 0.1f);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            direction = new Vector2(1f, 0.1f);
+        }
+
+        this.GetComponent<Rigidbody2D>().AddForce(direction * 2000f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag != "Enemy" && coll.gameObject.tag != "Dead")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
